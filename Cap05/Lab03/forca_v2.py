ef03673b3dea918701f9776bc7859f11d2ab5d59
gabipz <gabriela.pss@hotmail.com>
2020-07-10 15:24:37 -0300

    add file

---
+++ Cap05/Lab03/forca_v2.py
@@ -0,0 +1,129 @@
+# Hangman Game (Jogo da Forca) 
+# Programação Orientada a Objetos
+
+# Import
+import random
+
+# Board (tabuleiro)
+board = ['''
+
+>>>>>>>>>>Hangman<<<<<<<<<<
+
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+=========''']
+
+
+# Classe
+class Hangman:
+
+	# Método Construtor
+	def __init__(self, word):
+		self.word = word
+		
+	# Método para adivinhar a letra
+	def guess(self, letter):
+		self.letter = letter
+		letter = str(input('Insira uma letra: '))
+		if letter in word:
+			return  
+		
+	# Método para verificar se o jogo terminou
+	def hangman_over(self):
+		return self.hangman_won() or # se ja foram 6 tentativas
+		
+	# Método para verificar se o jogador venceu
+	def hangman_won(self):
+		
+
+	# Método para não mostrar a letra no board
+	def hide_word(self):
+		
+		
+	# Método para checar o status do game e imprimir o board na tela
+	def print_game_status(self):
+		
+
+# Função para ler uma palavra de forma aleatória do banco de palavras
+def rand_word():
+        with open("palavras.txt", "rt") as f:
+                bank = f.readlines()
+        return bank[random.randint(0,len(bank))].strip()
+
+
+# Função Main - Execução do Programa
+def main():
+
+	# Objeto
+	game = Hangman(rand_word())
+
+	# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
+	
+
+	# Verifica o status do jogo
+	game.print_game_status()	
+
+	# De acordo com o status, imprime mensagem na tela para o usuário
+	if game.hangman_won():
+		print ('\nParabéns! Você venceu!!')
+	else:
+		print ('\nGame over! Você perdeu.')
+		print ('A palavra era ' + game.word)
+		
+	print ('\nFoi bom jogar com você! Agora vá estudar!\n')
+
+# Executa o programa		
+if __name__ == "__main__":
+	main()
---
+++ Cap05/Lab03/teste_forca.py
@@ -0,0 +1,28 @@
+
+word = 'macarrao'
+
+# List Comprehension - colocar letras da palavra em lista
+lst = enumerate([x for x in word])
+
+
+
+letter = str(input('Insira uma letra: '))
+
+if letter in word:
+    
+else: 
+    print('não tem')
+
+"""
+if letter in word:
+    print('tem')
+else: 
+    print('não tem')
+"""
+
+tamanho = (len(lst))
+
+
+
+#print ('{:_tamanho}'.format())
+
